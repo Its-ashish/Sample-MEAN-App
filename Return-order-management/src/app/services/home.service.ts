323e76052ef4d00dd512ab2031d8e05eed75fe4a
Its-ashish <tashish531@gmail.com>
2022-05-23 17:38:22 +0530

    Project Added

---
+++ Return-order-management/src/app/services/home.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor(private http: HttpClient) { }
+
+  sendOrderDetails(body:any): Observable<any>{
+    const headers= {'Content-type': 'application/json'}
+    return this.http.post('http://localhost:5000/api/ComponentProc', body, { headers} );
+  }
+
+  updateStatus(status :any,requestId:Number): Observable<any>{
+    const headers= {'Content-type': 'application/json'}
+    return this.http.patch(`http://localhost:5000/api/ComponentProc/${requestId}`, status, {headers});
+  }
+}
